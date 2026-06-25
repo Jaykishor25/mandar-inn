9d8dfe157e0b4863994dd187d1331842d03d3ba9
Jaykishor25 <jaykishor2515.jnvbanka@gmail.com>
2026-06-25 10:52:26 +0530

    final link fixed

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,3 @@
 // client/src/config/api.js
 // Ye code apne aap detect karega ki aap local pe hain ya live website pe.
-export const API_BASE_URL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:5000' 
-  : 'https://mandar-inn.onrender.com'; // Ye temporary link hai
+export const API_BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000' : 'https://mandar-inn.onrender.com';
